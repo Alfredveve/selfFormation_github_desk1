7fa109acd35c5787fdb9d55b76c23cd4918df3c9
BEAVOGUI <vevebea2022@outlook.fr>
2024-07-08 06:17:48 +0000

    Ajout de la soustration
    j'ai ajouter la soustraction

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 def somme(a, b):
     return a + b
 print(somme(2, 3))
+
+# la différence / la soustration
+
+def soustration(x, y):
+    return x - y
+print("la soustration de 3 et 2 est : ",soustration(3,2))
